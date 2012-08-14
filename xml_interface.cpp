4284dccd2a765364e912d08d427ee9e0643b4a07
Michael Uman <muman613@gmail.com>
2012-08-14 00:30:51 -0700

    Added xml interface...

---
+++ xml_interface.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "xml_interface.h"
+
+bool xml_load_controlfile(const char* szControlName){
+    return false;
+}
---
+++ xml_interface.h
@@ -0,0 +1,8 @@
+#ifndef __XML_INTERFACE_H__
+#define __XML_INTERFACE_H__
+
+#include <libxml/tree.h>
+
+bool xml_load_controlfile(const char* szControlName);
+
+#endif // __XML_INTERFACE_H__
