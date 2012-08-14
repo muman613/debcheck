ebfc2d68b840ff1fe1c9d8a5fb164e3726508d2f
Michael Uman <muman613@gmail.com>
2012-08-14 00:07:12 -0700

    Added commandline parsing using popt library.

--- main.cpp
+++ main.cpp
@@ -13,17 +13,55 @@
 #include <iostream>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <popt.h>
 
 #include "dpkg_interface.h"
 
 using namespace std;
 
+char*       szControlFile = 0L;
+
+struct poptOption poptions[] = {
+    { "control", 'c', POPT_ARG_STRING, &szControlFile, 0, "Control file", "XML File" },
+    POPT_AUTOHELP
+    POPT_TABLEEND
+};
+
+/**
+ *  Parse the commandline using the popt library.
+ */
+
+bool parse_args(int argc, const char* argv[])
+{
+    bool        bRes = false;
+    poptContext poptCon;
+    char        rc;
+
+    poptCon = poptGetContext( "debcheck", argc, argv, poptions, 0 );
+
+    while ((rc = poptGetNextOpt( poptCon )) > 0) {
+        switch (rc) {
+        default:
+            break;
+        }
+    }
+
+    bRes = (szControlFile != 0);
+    poptFreeContext( poptCon );
+
+    return bRes;
+}
 
 /**
  *
  */
 
 int main(int argc, const char* argv[]) {
+    if (!parse_args( argc, argv )) {
+        return -10;
+    }
+
+    printf("Control file = %s\n", szControlFile);
 
     if (!dpkg_system_open()) {
         fprintf(stderr, "ERROR: Unable to open dpkg library!\n");
@@ -32,8 +70,13 @@ int main(int argc, const char* argv[]) {
 
     dpkg_dump_installed();
 
-    if (dpkg_is_package_installed("antiword", "0.37-6")) {
+    if (dpkg_is_package_installed("antiword", NULL)) {
+
         printf("Package is installed!\n");
+
+        if (dpkg_is_package_installed("antiword", "0.37-6")) {
+            printf("Package 0.37-6 is installed!\n");
+        }
     }
 
     dpkg_system_close();
