de0232bdffc0158d3e7929d50fe9eb22affcf471
Michael Uman <Michael_Uman@sigmadesigns.com>
2012-08-14 18:30:48 -0700

    Load basic control file [xml_interface.cpp]

---
+++ basicTypes.h
@@ -0,0 +1,12 @@
+#ifndef __BASICTYPES_H__
+#define __BASICTYPES_H__
+
+
+#include <string>
+#include <vector>
+
+typedef std::string STRING;
+typedef std::vector<STRING> STRING_VECTOR;
+
+#endif // __BASICTYPES_H__
+
