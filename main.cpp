6787f18ceec510a9bd6c5d7581d0d1a13f114d50
Michael Uman <muman613@gmail.com>
2012-08-15 23:17:13 -0700

    Added '-v' option to enable verbose output. Added count of installed, wrong  version, and uninstalled for statistics. Main loop to interate through all packages specified in the control file, checking if the package is installed.

--- main.cpp
+++ main.cpp
@@ -19,13 +19,19 @@
 #include "dpkg_interface.h"
 #include "xml_interface.h"
 #include "debPackageDB.h"
+#include "dbgutils.h"
 
 using namespace std;
 
-char*       szControlFile = 0L;
+char*       szControlFile   = 0L;
+bool        gVerbose        = false;
+
+/** Popt options array. */
 
 struct poptOption poptions[] = {
-    { "control", 'c', POPT_ARG_STRING, &szControlFile, 0, "Control file", "XML File" },
+    { "control", 'c', POPT_ARG_STRING, &szControlFile, 0,   "Control file", "XML File" },
+    { NULL,      'v', POPT_ARG_NONE,   NULL,           'v', "Enable verbose messages", },
+
     POPT_AUTOHELP
     POPT_TABLEEND
 };
@@ -44,6 +50,10 @@ bool parse_args(int argc, const char* argv[])
 
     while ((rc = poptGetNextOpt( poptCon )) > 0) {
         switch (rc) {
+        case 'v':
+            gVerbose = true;
+            break;
+
         default:
             break;
         }
@@ -61,6 +71,10 @@ bool parse_args(int argc, const char* argv[])
 
 int main(int argc, const char* argv[]) {
     debPackageDB    packageDB;
+    int             nRes = 0;
+    int             cntInstOK = 0,
+                    cntInstWrongVer = 0,
+                    cntNotInst = 0;
 
     if (!parse_args( argc, argv )) {
         return -10;
@@ -73,15 +87,70 @@ int main(int argc, const char* argv[]) {
         return -1;
     }
 
+    /* Load the control file, if failed print error and exit. */
     if (xml_load_controlfile( szControlFile, packageDB )) {
         printf("-- control file loaded ok!\n");
-        packageDB.Dump(stdout);
+
+        if (gVerbose)
+            packageDB.Dump(stdout);
 
 #ifdef _EXTRA_EXTRA_DEBUG
         dpkg_dump_installed();
 #endif
 
-        if (dpkg_is_package_installed("antiword", NULL)) {
+        DEB_PACKAGE_VECTOR_ITER pIter;
+        for (pIter = packageDB.begin() ; pIter != packageDB.end() ; pIter++) {
+            STRING          sPackName, sPackVers;
+            debPackage      pkg = *pIter;
+
+            sPackName = pkg.PackageName();
+            sPackVers = pkg.PackageVersion();
+
+            D(ebug("-- processing package [%s - %s]\n",
+                   sPackName.c_str(),
+                   sPackVers.c_str()));
+
+            if (dpkg_is_package_installed(sPackName.c_str(),
+                                          sPackVers.c_str()))
+            {
+                printf("Package %s version %s installed!\n",
+                       sPackName.c_str(), sPackVers.c_str());
+                cntInstOK++;
+            }
+            else
+            {
+                STRING sActualVers;
+
+                if (dpkg_get_package_version( sPackName.c_str(), sActualVers)) {
+                    printf("WARNING: Package %s installed BUT version %s NOT version %s!\n",
+                           sPackName.c_str(), sActualVers.c_str(), sPackVers.c_str());
+                    cntInstWrongVer++;
+                } else {
+                    printf("ERROR: Package %s IS NOT installed!\n",
+                           sPackName.c_str());
+                    cntNotInst++;
+                }
+            }
+        }
+
+        if (gVerbose) {
+            printf("STAT : %d Installed / %d Wrong Version / %d Not Installed\n",
+                   cntInstOK, cntInstWrongVer, cntNotInst);
+        }
+
+
+    } else {
+        fprintf(stderr, "ERROR: Unable to parse control file!\n");
+        nRes = -10;
+    }
+
+    dpkg_system_close();
+
+    return nRes;
+}
+
+#if 0
+        if (dpkg_is_package_installed("antiword")) {
 
             printf("Package is installed!\n");
 
@@ -93,10 +162,4 @@ int main(int argc, const char* argv[]) {
                 printf("antiword version %s is installed!\n", sVersion.c_str());
             }
         }
-    } else {
-    }
-
-    dpkg_system_close();
-
-    return 0;
-}
+#endif
